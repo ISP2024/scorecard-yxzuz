564ec83381f850681b456896ab533c0f40252aaf
Natcha Ranchan <natcha.ran@ku.th>
2024-09-16 14:24:28 +0700

    add type hints

--- scorecard.py
+++ scorecard.py
@@ -9,7 +9,12 @@ Add these hints ONE AT A TIME.  SAVE the file after each one and run mypy.
 Observe how the type hint helps it perform static checking.
 
 1) add type to parameter:                `add_score(self, score: float)`
+ANS: will got this warning:
+scorecard.py:67: error: Argument 1 to "add_score" of "Scorecard" has incompatible type "str"; expected "float"  [arg-type]
+
 2) add type to return value of average:  `average(self) -> ???`
+ANS: error: Unsupported operand types for + ("str" and "float")  [operator]
+
 3) add type to self.scores attribute:    `self.scores: ???[???] = []`
 4) add type hints for all parameters and return values.
    If a function does not return a value, don't write a type hint.
@@ -17,6 +22,7 @@ Observe how the type hint helps it perform static checking.
    Include the type of keys and values.
 
 """
+from typing import List, Iterator
 
 
 class Scorecard:
@@ -24,18 +30,23 @@ class Scorecard:
 
     def __init__(self):
         """Iniiialize a new Scorecard."""
-        self.scores = []
+        self.scores: List[float] = []
 
-    def add_score(self, score):
+    def add_score(self, score: float):
         """Add a score to the Scorecard."""
         self.scores.append(score)
 
-    def average(self):
+    def average(self)-> float:
         """Return the average of all scores, 0 if no scores."""
         return sum(self.scores)/max(1,len(self.scores))
 
+    def __len__(self)-> int:
+        return len(self.scores)
 
-def print_scores(score_card):
+    def __iter__(self)->Iterator[float]:
+        return iter(self.scores)
+
+def print_scores(score_card: Scorecard):
     """Print statistics for the scorecard and the actual scores."""
 
     # What changes to Scorecard are needed in order to make this code work?
@@ -46,12 +57,12 @@ def print_scores(score_card):
         print(score)
 
 
-def ordinal(num):
+def ordinal(num: int)-> str:
     """Return the ordinal value of an integer; works for numbers up to 20.
 
     For examples: ordinal(1) is '1st', ordinal(2) is '2nd'.
     """
-    suffixes = {1: "st", 2: "nd", 3: "rd"}
+    suffixes:dict[int,str]  = {1: "st", 2: "nd", 3: "rd"}
     return str(num) + suffixes.get(num, "th")
 
 
